0edde9f9493434f44735780d3acc1f2f8e843001
Iglisson Ruan <iglioliveira87@outlook.com>
2021-09-17 00:39:21 -0300

    :up: index muda a section

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+// endereços
+const cb_busca = document.getElementById("search");
+const cb_biblioteca = document.getElementById("biblioteca");
+
+// adicionando a classe ao hide-section
+cb_busca.addEventListener("click", function(){
+    // document.querySelector(".container").classList.toggle("hide-busca-section");
+    document.querySelector(".container").classList.remove("hide-busca-section");
+    document.querySelector(".container").classList.add("hide-biblioteca-section");
+    cb_biblioteca.checked = false;
+});
+    
+cb_biblioteca.addEventListener("click", function(){
+    // document.querySelector(".container").classList.toggle("hide-biblioteca-section");
+    document.querySelector(".container").classList.remove("hide-biblioteca-section");
+    document.querySelector(".container").classList.add("hide-busca-section");
+    cb_busca.checked = false;
+});
